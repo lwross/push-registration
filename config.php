142aa46c8f45b695541b6284e9d52c51283ac5bb
Lachlan Ross <lachlan.ross@salesforce.com>
2017-10-02 15:24:41 +1100

    no message

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     'appsignature' => 'none', 
-	'clientid' => getenv('APP_CENTER_API_INTEGRATION_CLIENTID'),//'j8o0682vcfjzd6embewqhpbc',
-	'clientsecret' => getenv('APP_CENTER_API_INTEGRATION_CLIENT_SECRET'),//'QkXBQHPuVcOZKGuEOoIYYxkr',
+	'clientid' => getenv('APP_CENTER_API_INTEGRATION_CLIENTID'),
+	'clientsecret' => getenv('APP_CENTER_API_INTEGRATION_CLIENT_SECRET'),
 	'defaultwsdl' => 'https://webservice.exacttarget.com/etframework.wsdl',
     'xmlloc' => './ExactTargetWSDL.xml',
 );
