4842c60f499dd969040398d4be5bd9998b15c559
Lachlan Ross <lachlan.ross@salesforce.com>
2017-10-03 08:01:01 +1100

    adding php generated manifest file

---
+++ manifest.json.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/manifest+json');
+echo json_encode($manifest);
+$manifest = [
+    "name" => "Push Demo",
+    "short_name" => "Push Demo",
+    "start_url" => "./index.html?homescreen=1",
+    "display" => "standalone",
+    "gcm_sender_id" => "82415342949",
+    "icons" => [
+        "src" => "images/icon-192x192.png",
+        "sizes"=> "192x192",
+        "type" => "image/png"
+    ]
+];
