0330d422bca569215f938f4720bf0e153c19bf8e
Lachlan Ross <lachlan.ross@salesforce.com>
2017-10-03 09:22:26 +1100

    use GCM_SENDER_ID from heroku env for gcm_sender_id

--- manifest.json.php
+++ manifest.json.php
@@ -1,15 +1,16 @@
 <?php
 header('Content-Type: application/manifest+json');
+$icons = (object) array("src" => "images/icon-192x192.png",
+        "sizes"=> "192x192",
+        "type" => "image/png");
 $manifest = [
     "name" => "Push Demo",
     "short_name" => "Push Demo",
     "start_url" => "./index.html?homescreen=1",
     "display" => "standalone",
-    "gcm_sender_id" => "82415342949",
+    "gcm_sender_id" => getenv('GCM_SENDER_ID');,
     "icons" => [
-        "src" => "images/icon-192x192.png",
-        "sizes"=> "192x192",
-        "type" => "image/png"
+        $icons
     ]
 ];
 echo json_encode($manifest);
